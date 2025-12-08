2e1bebb0da986b0f0ba0d231dd61423c0fba6542
charlesgchen <charlesg.chen@mail.utoronto.ca>
2025-12-07 22:36:47 -0500

    added visitor count functionality

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,17 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { NavLink } from "react-router-dom"
 function Header(){
-      const [count, setCount] = useState(0)
-
+    const [count, setCount] = useState(0)
+    useEffect(() =>{
+        fetch("https://8hiwdq6ggf.execute-api.us-east-1.amazonaws.com/visitor", {
+            method: "POST"
+        })
+        .then((response) => response.json())
+        .then((response) => setCount(response.count))
+        .catch((error) => console.log(error));
+    },[]);
     return(
+       
         <>
         <div className='"w-full h-16 bg-blend-overlay flex items-center justify-between px-8 shadow-md"'>
         <nav className="">
@@ -18,9 +26,9 @@ function Header(){
             Charles Chen Portfolio
             </NavLink>
         </nav>
-        <button onClick={() => setCount((count) => count + 1)}>
-          visitor count is {count}
-        </button>
+        <h1>
+            visitor count is {count}
+        </h1>
         <nav className="space-x-6">
             <NavLink
             to="/Work"
